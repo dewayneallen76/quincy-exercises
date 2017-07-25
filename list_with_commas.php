4c3ea6c58cd3292e364242b1fe1e6c265cdeee6e
Dewayne Allen <dewayneallen76@gmail.com>
2017-07-25 12:00:39 -0500

    exploded the string into an array

--- list_with_commas.php
+++ list_with_commas.php
@@ -2,6 +2,8 @@
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
+$physicistsArray = [];
+
 function humanizedList($string) {
   $physicistsArray = explode(',', $string);
   print_r($physicistsArray);
