c61faa2ef3121383c20d0e6dc51c0c4c9fd85fae
Dewayne Allen <dewayneallen76@gmail.com>
2017-07-25 11:19:33 -0500

    completed exercise

--- merge-arrays.php
+++ merge-arrays.php
@@ -4,17 +4,24 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-// 
+//
 // $items = ['First', 'Second', 'Third'];
 // array_push($items, 'Fourth', 'Fifth', 'Sixth');
 // print_r($items);
 
 
-function compareArrays($array1, $array2) {
-  $mergedArray = [];
-  foreach($array1 as $index => $name) {
-    echo $name . PHP_EOL;
+
+function combineArrays($array1, $array2)
+{
+	$merge = [];
+	foreach($array1 as $index => $name) {
+		array_push($merge, $name);
+	   if ($name != $array2[$index]) {
+        print_r($array2[$index]);
+		    array_push($merge, $array2[$index]);
+	    }
   }
+	print_r($merge);
 }
 
-compareArrays($names, $compare);
+combineArrays($names, $compare);
