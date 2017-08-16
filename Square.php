3c3c20cdd7e8cc9fea24090a14f8409f2b8f3374
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-16 11:03:24 -0500

    refactored for overriding lesson

--- Square.php
+++ Square.php
@@ -4,9 +4,19 @@ require_once 'Rectangle.php';
 
 class Square extends Rectangle
 {
+  public function __construct($height)
+  {
+    parent::__construct($height, $height);
+  }
+
   public function perimeter()
   {
-    return ($this->height + $this->width) * 2 . PHP_EOL;
+    return $this->height * 4;
+  }
+
+  public function area()
+  {
+    return $this->height * $this->height;
   }
 }
 
