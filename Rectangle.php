2db3b2674cd491abb1570fc392f7ac03bef0e027
Dewayne Allen <dewayneallen76@gmail.com>
2017-08-16 12:29:04 -0500

    refactored for visibility lesson. added getter and setter methods.

--- Rectangle.php
+++ Rectangle.php
@@ -3,12 +3,34 @@
 
 class Rectangle
 {
-  public $height;
-  public $width;
+  private $height;
+  private $width;
 
   public function __construct($height, $width)
   {
+    $this->setHeight($height);
+    $this->setWidth($width);
+  }
+
+  // Added getters
+  public function getHeight()
+  {
+    return $this->height;
+  }
+
+  public function getWidth()
+  {
+    return $this->width;
+  }
+
+  // Added setters
+  public function setHeight($height)
+  {
     $this->height = $height;
+  }
+
+  public function setWidth($width)
+  {
     $this->width = $width;
   }
 
